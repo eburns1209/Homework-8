f1a6f7d88a96b20920a8d2329d5b93095fc6a1dd
eburns <eburns1209@gmail.com>
2017-04-26 11:01:22 -0400

    I completed

--- liri.js
+++ liri.js
@@ -1,24 +1,17 @@
-// Using the require keyword lets us access all of the exports
-// in our ess.js file
-
 var keys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
-//var fs = require('fs');
+var fs = require('fs');
 
 console.log("type my-tweets, spotify-this-song, movie-this, or do-what-it-says, to get started");
 
-
-// var tweets = twitter.twitterKeys;
 var userCommand = process.argv[2];
 var secondCommand = process.argv[3];
 
 for(i=4; i < process.argv.length; i ++){
 	secondCommand += '+' + process.argv[i];
 }
-
-//console.log(secondCommand);
 
 function theActionSwitch(){
 	switch(userCommand){
@@ -39,85 +32,62 @@ function theActionSwitch(){
  		followTheTextbook();
  		break;
 
+ 		default:
+ 		console.log("LIRI does not know that");
+
 
 	}//close switch
 };//close actionSwitch
+
+var runThis = function(argOne, argTwo){
+	pick(argOne, argTwo);
+}//end runThis
 	
 function getTweets(){
 	console.log("These are my tweets");
 
-	var client = new twitter({
-
-		consumer_key: keys.twitterKeys.consumer_key,
-		consumer_secret: keys.twitterKeys.consumer_secret, 
-		access_token_key: keys.twitterKeys.access_token_key,
-		access_token_secret: keys.twitterKeys.access_token_secret
-	});
-
-
+	var client = new twitter(keys.twitterKeys);
+		
 	//parameters
-	var params = {
-		screen_name: 'Runtrigirl'} &&
-		{count: 20
-	};
-
+	var params = {screen_name: 'Runtrigirl'} 
+		&&	{count: 20 };
 
 	//call get method on our client variable twitter instance
-	//client.get(path, params, callback)
+	
 	client.get('statuses/user_timeline', params, function(error, tweets, response){
 		if(!error) {
 			 //console.log(error);
-			 console.log(tweets);
+			 for(var i =0; i < tweets.length; i++){
+			 	console.log(tweets[i].created_at);
+			 	console.log(' ');
+			 	console.log(tweets[i].text);
+			 }
+			 
 			}
-		else{
-			throw error
-		}
-			 console.log(response);
-			var tweets = tweets.trim(", ");
-			console.log(tweets);
-			// for(i=0; i<tweets.length; i++){
-			// var returnedData = ('Number: ' + (i+1) + '\n' + tweets[i].created_at + '\n' + tweet[i].text + '\n');
-			//  console.log(returnedData);
-			//  console.log("----------------");
-			//    }
+		
 		});
-	 //});
+			
 };//end getTweets
 
-//};//close switch here or line 43
 
 var getArtistNames = function(artists){
 	return artists.name;
 }
+
 function spotifyMe(song){
 
 	console.log("Music for DAYS!");
-//something to look at from instructor
-// spotify.search({type:"track", query: 'dancing in the moonlight'}, function(err, data){
-// 	if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
-    // else{
-    // 	var songInfo = data.tracks.item[0];
-    // 	var songResult = console.log(songInfo.artists[0].name);
-    // 					console.log(songInfo.name);
-    // }
-// console.log(data.tracks.items[0]);
-// 
-//  }
-//  });
 	//variable for search term, test if defind
 	 var searchTrack;
 	 if (secondCommand === undefined){
 	searchTrack = "What's My Age Again?";
 	 }else{
 	 	searchTrack = secondCommand;
-	 }
+	 	  }
 
 	//launch spotify search
 
-	 spotify.search({type:"track", query:searchTrack}, function(err, data){
+	spotify.search({type:"track", query:searchTrack}, function(err, data){
 		if (err){
 			console.log("Error occurred: " + err);
 			return;
@@ -125,19 +95,18 @@ function spotifyMe(song){
 		else{
 
 			var songs = data.tracks.items;
-//console.log(songs);
- for (var i = 0; i < songs.length; i++) {
+			//console.log(songs);
+ 			for (var i = 0; i < songs.length; i++) {
    
-// // use the various properties on songs[i] here
+			 // use the various properties on songs[i] here
 			console.log("Artist(s): " + songs[i].artists.map(getArtistNames));
 			console.log("Song name: " + songs[i].name);
 		    console.log("Album: " + songs[i].album.name);
 		    console.log("Preview song: " + songs[i].preview_url);
-		 }
+		    
+		 	}
 		}
 	});
-
-
 
 };//end spotifyme
 
@@ -151,7 +120,7 @@ function aMovieForMe(){
 		searchMovie = "Mr. Nobody";
 	}else{
 		searchMovie = secondCommand;
-	};
+		};
 
 	var url = 'http://www.omdbapi.com/?t=' + searchMovie + ' &y=&plot=long&tomatoes=true&r=json';
 
@@ -176,27 +145,27 @@ function aMovieForMe(){
 function followTheTextbook(){
 	console.log("Looking at random.txt now");
 
-	var fs = require('fs');
-
-	fs.readfile("random.txt", "utf8", function(error, data){
+	fs.readFile('random.txt', 'utf8', function (error, data){
+  		if (error) {
+  			//throw err;
+		  return console.log(error);
+		}
 		console.log(data);
-		if (error){
-			console.log(error);
-			}else
-				{
-				//split data, declare variables
-				var dataArr = data.split(', ');
-				userCommand = dataArr[0];
-				secondCommand = dataArr[1];
+		// else{
+		var dataArr = data.split(', ');
+		userCommand = dataArr[0];
+		secondCommand = dataArr[1];
 
-				//multi-word search term add
-				for (i = 2; i < dataArr.length; i++){
-					secondCommand = secondCommand + " + " + dataArr[i];
-				};
-				//run Action
-				theActionSwitch();
-			};//end else
-	});//end read file
+		for(i=2; i<dataArr.length; i++){
+			secondCommand = secondCommand + " + " + dataArr[i];
+			};
+		
+		 theActionSwitch();
+
+	});
+
+				
+			
 };//end followTheTextbook
 
 
